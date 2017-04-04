c7b7cf520dd062df6c3bc804cfe9b29013cbdbba
Samuel Altarac <samuel.altarac@gmail.com>
2017-04-04 08:48:51 -0400

    testing ajax and php
    many of these changes are just to test functionality.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Oreo</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -17,16 +17,110 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     <script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-MML-AM_CHTML'></script>
+
     <script
-  src="https://code.jquery.com/jquery-3.2.1.min.js"
-  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  src="https://code.jquery.com/jquery-3.2.1.js"
+  integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
   crossorigin="anonymous"></script>
+
+  <!-- <script src="jquery-3.2.0.min.js"></script> -->
+
   </head>
   <body class="container-fluid">
-    <h1 class="text-center">Homework:</h1>
-    <div class="col-md-6 col-md-offset-3">
-      hello world!!
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Oreo</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Link</a></li>
+            <!-- <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li> -->
+          </ul>
+          <!-- <form class="navbar-form navbar-left">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form> -->
+
+
+          <!-- <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+              </ul>
+            </li>
+          </ul> -->
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
+
+    <div>
+      <a class="btn btn-sm btn-primary" id="hw">Do homework</a>
     </div>
+
+
+    <!-- questions are loaded here -->
+    <div class="col-md-6 col-md-offset-3" id="homeworkScreen">
+      <div class="panel panel-default">
+        <div class="panel-body">
+        solve the following equations $$2+x=4$$
+        </div>
+        <div class="panel-footer">
+          <a class="btn btn-xsm btn-danger">back</a>
+          <a class="btn btn-xsm btn-success pull-right">next</a>
+        </div>
+      </div>
+    </div>
+    <!-- ***************** -->
+    <?php
+
+     $host = "localhost";
+     $user = "root";
+     $pass = "root";
+
+     mysql_connect($host, $user, $pass);
+     mysql_select_db('oreodb');
+
+     $select_questions = "SELECT * FROM bank WHERE id=1";
+
+    $s = mysql_query($select_questions);
+
+    while ($d = mysql_fetch_assoc($s)) {
+      echo $d['text'];
+    }
+
+    ?>
+
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -37,10 +131,33 @@
 
       
       function renderMath() {
+
         $("body").click(function() {
         MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-      })
+      });
+
       }
+
+      $("#hw").click(function() {
+        $("#homeworkScreen").;
+      });
+
+
+      var bank = {
+        1 : {
+          type : "multiple choice",
+          text : ,
+          options : [1, 2, 3, 4],
+          solution: 2
+        },
+        2 : {
+          type : "multiple choice",
+          text : "solve for x. $$3+x=5$$",
+          options : [1, 2, 3, 4],
+          solution: 2
+        }
+      }
+
 
       
     </script>
